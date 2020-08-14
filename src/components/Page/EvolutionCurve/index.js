850508842bb37c0ebac6b6e065372d27fbee6b3a
MkDs17 <mickael.rassicot@gmail.com>
2020-08-14 11:31:12 +0200

    update according to eslint

--- src/components/Page/EvolutionCurve/index.js
+++ src/components/Page/EvolutionCurve/index.js
@@ -1,21 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Pie, Line } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import _ from 'lodash';
 
 import './evolution-curve.scss';
 
-import { fakerChartsLineDatas } from '../../../utils/functions';
+import { chartsLineDatas } from '../../../utils/functions';
 
 function EvolutionCurve({ dailySummary }) {
   const [chartLineDatas, setChartLineDatas] = useState([]);
 
   useEffect(() => {
-    {!_.isEmpty(dailySummary) && 
-      setChartLineDatas(fakerChartsLineDatas(dailySummary));
-    }
+    {!_.isEmpty(dailySummary) && setChartLineDatas(chartsLineDatas(dailySummary));}
   }, [dailySummary]);
-
-  console.log('chartLineDatas', chartLineDatas)
 
   return (
     <div id="evolution-curve">
@@ -23,12 +19,10 @@ function EvolutionCurve({ dailySummary }) {
         <div className="evolution-curve-title">
           <h2><span>Evolution Curves</span> </h2>
         </div>
-          <div className="evolution-curve-content">
-            {!_.isEmpty(chartLineDatas) && 
-              //console.log('chartLineDatas here man', chartLineDatas)
-              <Line data={chartLineDatas} />
-            }
-          </div>
+        <div className="evolution-curve-content">
+          { !_.isEmpty(chartLineDatas)
+            && <Line data={chartLineDatas} /> }
+        </div>
       </div>
     </div>
   );
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -4,58 +4,53 @@ const thousands_separators = (num) => {
   const num_parts = num.toString().split(".")
   num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ")
   return num_parts.join(".")
-}
+};
 
 export const getNameOfActiveCountry = (countries, activeCountry) => {
   let country = countries.find(country => country.value === activeCountry);
   return country.text
-}
+};
 
 export const getThreeMostAffected = (stats, countriesArray) => {
+  const threeMostAffected = [];
 
-  let threeMostAffected = []
+  const reuniteDataByCountry = [];
 
-  let mostConfirmed;
-  let mostRecovered;
-  let mostDeaths;
-
-  let reuniteDataByCountry = [];
-  
-  let totalSumConfirmedByCountry = [];
-  let totalSumRecoveredByCountry = [];
-  let totalSumDeathsByCountry = [];
+  const totalSumConfirmedByCountry = [];
+  const totalSumRecoveredByCountry = [];
+  const totalSumDeathsByCountry = [];
 
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-  countriesArray.map(option => {
+  countriesArray.map((option) => {
     // order array with all countryRegion in same array
-    let sameCountry = stats.filter(country => country.iso3 === option.value)
+    const sameCountry = stats.filter((country) => country.iso3 === option.value);
     // verfiy some array aren't empty
-    let verifyEmptyness = !_.isEmpty(sameCountry)
+    const verifyEmptyness = !_.isEmpty(sameCountry);
     // if array aren't empty, then push em into reunitedDataCountry arrat
     if (verifyEmptyness === true) {
-      reuniteDataByCountry.push(sameCountry)
+      reuniteDataByCountry.push(sameCountry);
     }
   })
 
   if (reuniteDataByCountry !== undefined) {
-    reuniteDataByCountry.map(array => {
-      let countConfirmed = []
-      let countRecovered = []
-      let countDeaths = []
+    reuniteDataByCountry.map((array) => {
+      const countConfirmed = [];
+      const countRecovered = [];
+      const countDeaths = [];
 
-      array.map(country => {
-        countConfirmed.push(country.confirmed)
-        countRecovered.push(country.recovered)
-        countDeaths.push(country.deaths)
+      array.map((country) => {
+        countConfirmed.push(country.confirmed);
+        countRecovered.push(country.recovered);
+        countDeaths.push(country.deaths);
       });
 
-      const sumConfirmed = countConfirmed.reduce(reducer)
-      const sumRecovered = countRecovered.reduce(reducer)
-      const sumDeaths = countDeaths.reduce(reducer)
+      const sumConfirmed = countConfirmed.reduce(reducer);
+      const sumRecovered = countRecovered.reduce(reducer);
+      const sumDeaths = countDeaths.reduce(reducer);
 
       // Verifier que la propriete flag contient bien une valeur, le cas écheant on passe la valeur en minuscule 
-      const flag  = array[0].iso2
+      const flag = array[0].iso2;
       if (flag !== undefined) {
         const flagToLowerCase = flag.toLowerCase();
 
@@ -86,102 +81,102 @@ export const getThreeMostAffected = (stats, countriesArray) => {
 
   totalSumConfirmedByCountry.map((country) => {
     // If firstConfirmed is Empty then put a country in it
-    if(_.isEmpty(firstConfirmed)) {
-      firstConfirmed = country
+    if (_.isEmpty(firstConfirmed)) {
+      firstConfirmed = country;
     }
-    
-    // Pick the 1st worst State and put object on firstConfirmed variable
-    else if( country.total > firstConfirmed.total) {
-      firstConfirmed = country
-    };
-  })
 
-  totalSumConfirmedByCountry.map(country => {
+    // Pick the 1st worst State and put object on firstConfirmed variable
+    else if (country.total > firstConfirmed.total) {
+      firstConfirmed = country;
+    }
+  });
+
+  totalSumConfirmedByCountry.map((country) => {
     // If secondConfirmed is Empty then put a country in it
-    if(_.isEmpty(secondConfirmed)) {
-      secondConfirmed = country
-    } 
+    if (_.isEmpty(secondConfirmed)) {
+      secondConfirmed = country;
+    }
 
     // Pick the 2nd worst State and put object on secondConfirmed variable
-    else if( country.total < firstConfirmed.total && country.total > secondConfirmed.total ) {
-      secondConfirmed = country
+    else if (country.total < firstConfirmed.total && country.total > secondConfirmed.total) {
+      secondConfirmed = country;
     }
-  })
+  });
 
-  totalSumConfirmedByCountry.map(country => {
+  totalSumConfirmedByCountry.map((country) => {
     // If thirdConfirmed is Empty then put a country in it
-    if(_.isEmpty(thirdConfirmed)) {
-      thirdConfirmed = country
-    } 
+    if (_.isEmpty(thirdConfirmed)) {
+      thirdConfirmed = country;
+    }
 
     // Pick the 3nd worst State and put object on secondConfirmed variable
-    else if( country.total < secondConfirmed.total && country.total > thirdConfirmed.total ) {
+    else if (country.total < secondConfirmed.total && country.total > thirdConfirmed.total) {
       thirdConfirmed = country
     }
-  })
+  });
 
-  mostConfirmed = {
+  const mostConfirmed = {
     first: firstConfirmed,
     second: secondConfirmed,
     third: thirdConfirmed,
-  }
-  threeMostAffected.push(mostConfirmed)
+  };
+  threeMostAffected.push(mostConfirmed);
 
   /********* CONFIRMED PART *********/
 
-  
+
 
   /********* RECOVERED PART *********/
-  
+
   let firstRecovered = [];
   let secondRecovered = [];
   let thirdRecovered = [];
 
-  totalSumRecoveredByCountry.map(country => {
+  totalSumRecoveredByCountry.map((country) => {
     // If firstRecovered is Empty then put a country in it
-    if(_.isEmpty(firstRecovered)) {
-      firstRecovered = country
-    } 
-    
-    // Pick the 1st worst State and put object on firstRecovered variable
-    else if( country.total > firstRecovered.total) {
-      firstRecovered = country
+    if (_.isEmpty(firstRecovered)) {
+      firstRecovered = country;
     }
-  })
 
-  totalSumRecoveredByCountry.map(country => {
+    // Pick the 1st worst State and put object on firstRecovered variable
+    else if (country.total > firstRecovered.total) {
+      firstRecovered = country;
+    }
+  });
+
+  totalSumRecoveredByCountry.map((country) => {
     // If secondRecovere is Empty then put a country in it
-    if(_.isEmpty(secondRecovered)) {
-      secondRecovered = country
-    } 
+    if (_.isEmpty(secondRecovered)) {
+      secondRecovered = country;
+    }
 
     // Pick the 2nd worst State and put object on secondRecovere variable
-    else if( country.total < firstRecovered.total && country.total > secondRecovered.total ) {
-      secondRecovered = country
+    else if (country.total < firstRecovered.total && country.total > secondRecovered.total) {
+      secondRecovered = country;
     }
-  })
+  });
 
-  totalSumRecoveredByCountry.map(country => {
+  totalSumRecoveredByCountry.map((country) => {
     // If thirdRecovere is Empty then put a country in it
-    if(_.isEmpty(thirdRecovered)) {
-      thirdRecovered = country
-    } 
+    if (_.isEmpty(thirdRecovered)) {
+      thirdRecovered = country;
+    }
 
     // Pick the 3nd worst State and put object on secondRecovere variable
-    else if( country.total < secondRecovered.total && country.total > thirdRecovered.total ) {
-      thirdRecovered = country
+    else if (country.total < secondRecovered.total && country.total > thirdRecovered.total) {
+      thirdRecovered = country;
     }
-  })
+  });
 
-  mostRecovered = {
+  const mostRecovered = {
     first: firstRecovered,
     second: secondRecovered,
     third: thirdRecovered,
-  }
-  threeMostAffected.push(mostRecovered)
+  };
+  threeMostAffected.push(mostRecovered);
 
   /********* RECOVERED PART *********/
-  
+
 
 
   /********* DEATHS PART *********/
@@ -190,54 +185,52 @@ export const getThreeMostAffected = (stats, countriesArray) => {
   let secondDeaths = [];
   let thirdDeaths = [];
 
-  totalSumDeathsByCountry.map(country => {
+  totalSumDeathsByCountry.map((country) => {
     // If firstDeaths is Empty then put a country in it
-    if(_.isEmpty(firstDeaths)) {
-      firstDeaths = country
-    } 
-    
-    // Pick the 1st worst State and put object on firstDeaths variable
-    else if( country.total > firstDeaths.total) {
-      firstDeaths = country
+    if (_.isEmpty(firstDeaths)) {
+      firstDeaths = country;
     }
-  })
 
-  totalSumDeathsByCountry.map(country => {
+    // Pick the 1st worst State and put object on firstDeaths variable
+    else if (country.total > firstDeaths.total) {
+      firstDeaths = country;
+    }
+  });
+
+  totalSumDeathsByCountry.map((country) => {
     // If secondDeaths is Empty then put a country in it
-    if(_.isEmpty(secondDeaths)) {
-      secondDeaths = country
-    } 
+    if (_.isEmpty(secondDeaths)) {
+      secondDeaths = country;
+    }
 
     // Pick the 2nd worst State and put object on secondDeaths variable
-    else if( country.total < firstDeaths.total && country.total > secondDeaths.total ) {
-      secondDeaths = country
+    else if (country.total < firstDeaths.total && country.total > secondDeaths.total) {
+      secondDeaths = country;
     }
-  })
+  });
 
-  totalSumDeathsByCountry.map(country => {
+  totalSumDeathsByCountry.map((country) => {
     // If thirdDeaths is Empty then put a country in it
-    if(_.isEmpty(thirdDeaths)) {
-      thirdDeaths = country
-    } 
+    if (_.isEmpty(thirdDeaths)) {
+      thirdDeaths = country;
+    }
 
     // Pick the 3nd worst State and put object on secondDeaths variable
-    else if( country.total < secondDeaths.total && country.total > thirdDeaths.total ) {
-      thirdDeaths = country
+    else if (country.total < secondDeaths.total && country.total > thirdDeaths.total) {
+      thirdDeaths = country;
     }
-  })
+  });
 
-  mostDeaths = {
+  const mostDeaths = {
     first: firstDeaths,
     second: secondDeaths,
     third: thirdDeaths,
-  }
-  threeMostAffected.push(mostDeaths)
+  };
+  threeMostAffected.push(mostDeaths);
 
   /********* DEATHS PART *********/
 
-
   return threeMostAffected;
-  
 };
 
 export const getPourcentageEvolution = (stats, dailyStats, countriesArray, activeCountry) => {
@@ -248,132 +241,110 @@ export const getPourcentageEvolution = (stats, dailyStats, countriesArray, activ
   const reuniteDailyDataByCountry = [];
 
   let totalEvolution = {};
-  //let evolution = {}
 
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  
-  if (countriesArray !== null && stats !== null ) {
+
+  if (countriesArray !== null && stats !== null) {
     // Allows to gather all the data concerning each country in an array
 
     if (activeCountry.name === 'Global') {
-      countriesArray.map(option => {
+      countriesArray.map((option) => {
         // order array with all countryRegion in same array for Total Datas
-        sameCountry = stats.filter(country => country.countryRegion === option.name)
-        dailySameCountry = dailyStats.filter(country => country.countryRegion === option.name)
-        
+        sameCountry = stats.filter((country) => country.countryRegion === option.name);
+        dailySameCountry = dailyStats.filter((country) => country.countryRegion === option.name);
+
         // verify some array aren't empty
-        let verifyEmptyness = !_.isEmpty(sameCountry)
+        const verifyEmptyness = !_.isEmpty(sameCountry);
         // if array aren't empty, then push em into reunitedDataCountry array
         if (verifyEmptyness === true) {
-          reuniteTotalDataByCountry.push(sameCountry)
-        }
-  
-        let dailyVerifyEmptyness = !_.isEmpty(dailySameCountry)
-        if (dailyVerifyEmptyness === true) {
-          reuniteDailyDataByCountry.push(dailySameCountry)
+          reuniteTotalDataByCountry.push(sameCountry);
         }
 
-      })
+        const dailyVerifyEmptyness = !_.isEmpty(dailySameCountry);
+        if (dailyVerifyEmptyness === true) {
+          reuniteDailyDataByCountry.push(dailySameCountry);
+        }
+      });
     }
 
     if (activeCountry.name !== 'Global') {
       // order array with all countryRegion in same array for Daily Datas
-      sameCountry = stats.filter(country => country.countryRegion === activeCountry.name)
-      dailySameCountry = dailyStats.filter(country => country.countryRegion === activeCountry.name)
+      sameCountry = stats.filter((country) => country.countryRegion === activeCountry.name);
+      dailySameCountry = dailyStats.filter((country) => country.countryRegion === activeCountry.name);
 
       // verify some array aren't empty
-      let verifyEmptyness = !_.isEmpty(sameCountry)
+      const verifyEmptyness = !_.isEmpty(sameCountry);
       // if array aren't empty, then push em into reunitedDataCountry array
       if (verifyEmptyness === true) {
-        reuniteTotalDataByCountry.push(sameCountry)
+        reuniteTotalDataByCountry.push(sameCountry);
       }
 
-      let dailyVerifyEmptyness = !_.isEmpty(dailySameCountry)
+      const dailyVerifyEmptyness = !_.isEmpty(dailySameCountry);
       if (dailyVerifyEmptyness === true) {
-        reuniteDailyDataByCountry.push(dailySameCountry)
+        reuniteDailyDataByCountry.push(dailySameCountry);
       }
     }
   }
-
-  //console.log('reuniteTotalDataByCountry', reuniteTotalDataByCountry)
-  //console.log('reuniteDailyDataByCountry', reuniteDailyDataByCountry)
 
   if (reuniteTotalDataByCountry !== null && reuniteDailyDataByCountry !== null) {
-    let totalCountConfirmed = []
-    let totalCountRecovered = []
-    let totalCountDeaths = []
-    let dailyCountConfirmed = []
-    let dailyCountRecovered = []
-    let dailyCountDeaths = []
+    const totalCountConfirmed = [];
+    const totalCountRecovered = [];
+    const totalCountDeaths = [];
+    const dailyCountConfirmed = [];
+    const dailyCountRecovered = [];
+    const dailyCountDeaths = [];
 
     // Handle the TOTAL Data array by country
-    reuniteTotalDataByCountry.map(array => {
-      array.map(country => {
-        totalCountConfirmed.push(country.confirmed)
-        totalCountRecovered.push(country.recovered)
-        totalCountDeaths.push(country.deaths)
-      })
-    })
+    reuniteTotalDataByCountry.map((array) => {
+      array.map((country) => {
+        totalCountConfirmed.push(country.confirmed);
+        totalCountRecovered.push(country.recovered);
+        totalCountDeaths.push(country.deaths);
+      });
+    });
 
     // Handle the DAILY Data array by country
-    reuniteDailyDataByCountry.map(array => {
-      array.map(country => {
-        dailyCountConfirmed.push(Number(country.confirmed))
-        dailyCountRecovered.push(Number(country.recovered))
-        dailyCountDeaths.push(Number(country.deaths))
-      })
-    })
+    reuniteDailyDataByCountry.map((array) => {
+      array.map((country) => {
+        dailyCountConfirmed.push(Number(country.confirmed));
+        dailyCountRecovered.push(Number(country.recovered));
+        dailyCountDeaths.push(Number(country.deaths));
+      });
+    });
 
     if ( !_.isEmpty(totalCountConfirmed) && !_.isEmpty(totalCountRecovered) && !_.isEmpty(totalCountDeaths) && !_.isEmpty(dailyCountConfirmed) && !_.isEmpty(dailyCountRecovered) && !_.isEmpty(dailyCountDeaths)) {
-      
-      const sumTotalConfirmed = totalCountConfirmed.reduce(reducer)
-      const sumTotalRecovered = totalCountRecovered.reduce(reducer)
-      const sumTotalDeaths = totalCountDeaths.reduce(reducer)
-      const sumDailyConfirmed = dailyCountConfirmed.reduce(reducer)
-      const sumDailyRecovered = dailyCountRecovered.reduce(reducer)
-      const sumDailyDeaths = dailyCountDeaths.reduce(reducer)
-      
-      /* if(sumTotalConfirmed !== 0 && sumTotalRecovered !== 0 && sumTotalDeaths !== 0 && sumDailyConfirmed !== 0 && sumDailyRecovered !== 0 && sumDailyDeaths !== 0 ) {
-        evolution = {
-          confirmed : Math.round(((sumTotalConfirmed - sumDailyConfirmed) / sumDailyConfirmed) * 100),
-          recovered : Math.round(((sumTotalRecovered - sumDailyRecovered) / sumDailyRecovered) * 100),
-          deaths : Math.round(((sumTotalDeaths - sumDailyDeaths) / sumDailyDeaths) * 100),
-        }
-      } */
+      const sumTotalConfirmed = totalCountConfirmed.reduce(reducer);
+      const sumTotalRecovered = totalCountRecovered.reduce(reducer);
+      const sumTotalDeaths = totalCountDeaths.reduce(reducer);
+      const sumDailyConfirmed = dailyCountConfirmed.reduce(reducer);
+      const sumDailyRecovered = dailyCountRecovered.reduce(reducer);
+      const sumDailyDeaths = dailyCountDeaths.reduce(reducer);
 
       totalEvolution = {
-        confirmed : Math.round(((sumTotalConfirmed - sumDailyConfirmed) / sumDailyConfirmed) * 100),
-        recovered : Math.round(((sumTotalRecovered - sumDailyRecovered) / sumDailyRecovered) * 100),
-        deaths : Math.round(((sumTotalDeaths - sumDailyDeaths) / sumDailyDeaths) * 100),
-      }
+        confirmed: Math.round(((sumTotalConfirmed - sumDailyConfirmed) / sumDailyConfirmed) * 100),
+        recovered: Math.round(((sumTotalRecovered - sumDailyRecovered) / sumDailyRecovered) * 100),
+        deaths: Math.round(((sumTotalDeaths - sumDailyDeaths) / sumDailyDeaths) * 100),
+      };
     }
-    //console.log('evolution', evolution);
-    console.log('total evolution', totalEvolution);
   }
 
-  
   if (!_.isEmpty(totalEvolution)) {
-    return totalEvolution
+    return totalEvolution;
   }
-
 };
 
-export const fakerChartsLineDatas = ( dailySummaryData ) => {
-  
+export const chartsLineDatas = (dailySummaryData) => {
   let data;
   let chartLineDatas;
 
-  console.log('dailySummaryData', dailySummaryData)
-
   if (!_.isEmpty(dailySummaryData)) {
-
     data = [{
-      labels: dailySummaryData.map(country => country.reportDate),
-      dataConfirmed: dailySummaryData.map(country => country.confirmed.total),
-      dataDeaths: dailySummaryData.map(country => country.deaths.total),
-    }]
+      labels: dailySummaryData.map((country) => country.reportDate),
+      dataConfirmed: dailySummaryData.map((country) => country.confirmed.total),
+      dataDeaths: dailySummaryData.map((country) => country.deaths.total),
+    }];
   }
-  
+
   if (!_.isEmpty(data)) {
     chartLineDatas = {
       labels: data[0].labels,
@@ -419,10 +390,9 @@ export const fakerChartsLineDatas = ( dailySummaryData ) => {
           pointRadius: 1,
           pointHitRadius: 10,
           data: data[0].dataDeaths,
-        }, 
+        },
       ],
     };
   }
-  console.log('chartLineDatas', chartLineDatas)
   return chartLineDatas;
 };
